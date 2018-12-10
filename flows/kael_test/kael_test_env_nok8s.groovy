651bb93e606731a693718e1107e89cce740191eb
Kael Zhan <kzhan@kzhanmbp.local>
2018-12-10 18:12:26 +0800

    test

--- flows/kael_test/kael_test_env_nok8s.groovy
+++ flows/kael_test/kael_test_env_nok8s.groovy
@@ -1,46 +1,28 @@
-@Library('my_lib') pipelineLibrary
-
-pipeline {
-    /*triggers {
-        issueCommentTrigger('build4mepls')
-        token: 'build4mepls'
-    }*/
-    agent any
-
-    parameters {
-        string(name: 'client_id', defaultValue: '500000001', description: 'clinet_id')
-        string(name: 'env', defaultValue: 'playground', description: 'environment')
-        string(name: 'JENKINS_EXECUTION_ID', defaultValue: 'xxxxxxx', description: 'job id')
+pipelineJob('kael_test_env_nok8s') {
+    authenticationToken('secret')
+    batchTask('upload', 'curl --upload-file build/dist.zip http://www.example.com/upload')
+    blockOnUpstreamProjects()
+    blockOnDownstreamProjects()
+    checkoutRetryCount(10)
+    concurrentBuild()
+    customWorkspace('/tmp')
+    jdk('jdk-1.8')
+    label('linux')
+    lockableResources('lock-resource')
+    multiscm {
+         // ...
     }
-
-    stages {
-        stage('kael test step 1') {
-            steps {
-                /*withContext{
-                    token: 'build4mepls'
-                }
-                configure { Node project ->
-                    project / authToken("myToken")
-                }*/
-                authenticationToken('secret')
-
-                sh 'env'
-                script{
-                    learn_s3_helper.parameter_save()
-                }
-                sh 'env'
-                script{
-                    learn_s3_helper.load_env_from_file(env.WORKSPACE+"/flows/kael_test/us-east-1-playground-fleet01.properties")
-                    learn_s3_helper.env_set("env","test")
-                    learn_s3_helper.env_set("host_name","blackboard.com")
-                }
-                sh 'env'
-                script{
-                    learn_s3_helper.env_save()
-                }
-                sh 'env'
-            }
-        }
+    publishers {
+         // ...
+    }
+    scm {
+         // ...
+    }
+    steps {
+         // ...
+    }
+    weight(50)
+    wrappers {
+         // ...
     }
 }
-
