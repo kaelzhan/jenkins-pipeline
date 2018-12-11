14269c300f2ed0a76a687301626fc7919b63fe14
Kael Zhan <kzhan@kzhanmbp.local>
2018-12-11 15:12:22 +0800

    test

--- vars/captain_notifier.groovy
+++ vars/captain_notifier.groovy
@@ -1,16 +1,5 @@
 #!/usr/bin/env groovy
 
 def captain_call(){
-    jobDsl scriptText: '''
-    pipelineJob('kael_test_env_nok8s/job/master') {
-        notifications {
-            endpoint(\'http://jenkins:lko34kd9fd2@captain.bbpd.io/api/jenkins/callback\') {
-                event(\'all\')
-                timeout(10000)
-                logLines(1)
-            }
-        }
-        authenticationToken("build4mepl")
-    }
-    '''
+
 }
