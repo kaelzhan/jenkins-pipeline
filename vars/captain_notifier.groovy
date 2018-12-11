233cc7ba10b9e5d0f9d206fd2a3b05e10df59a5f
Kael Zhan <kzhan@kzhanmbp.local>
2018-12-12 00:32:29 +0800

    test

--- vars/captain_notifier.groovy
+++ vars/captain_notifier.groovy
@@ -1,12 +1,13 @@
 #!/usr/bin/env groovy
 
 import env_config.Pipeline_Parameters
+import groovy.json.JsonOutput
 
 def create_captain_call_file(){
     def captain_callback_file = env.WORKSPACE + "/" + Pipeline_Parameters.captain_callback_file_name
     def captain_json = [: ]
 
-    def pipeline_jobname = "job/" + env.JOB_NAME.toString().split('/')[0] + "/job/" + env.JOB_NAME.toString().split('/')[1] + '/'
+    /*def pipeline_jobname = "job/" + env.JOB_NAME.toString().split('/')[0] + "/job/" + env.JOB_NAME.toString().split('/')[1] + '/'
     captain_json.("name") = env.JOB_NAME.toString()
     captain_json.("url") = pipeline_jobname.toString()
     captain_json.("build") = [: ]
@@ -14,9 +15,9 @@ def create_captain_call_file(){
     captain_json.("build").("number") = env.BUILD_ID.toString()
     captain_json.("build").("phase") = "STARTED"
     captain_json.("build").("status") = "SUCCESS"
-    captain_json.("build").("parameters") = [: ]
+    captain_json.("build").("parameters") = [: ]*/
     for (p in params){
-        captain_json.("build").("parameters").(p.key.toString()) = (p.value.toString())
+        captain_json.(p.key.toString()) = (p.value.toString())
     }
     writeJSON(file: captain_callback_file, json: captain_json, pretty: 4)
 }
