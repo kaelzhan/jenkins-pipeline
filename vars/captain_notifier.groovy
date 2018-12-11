6ad3ae59e2b83fe2cae74d649d1492b75a47b0ef
Kael Zhan <kzhan@kzhanmbp.local>
2018-12-11 23:59:51 +0800

    test

--- vars/captain_notifier.groovy
+++ vars/captain_notifier.groovy
@@ -9,7 +9,7 @@ def create_captain_call_file(){
     def pipeline_jobname = "job/" + env.JOB_NAME.toString().split('/')[0] + "/job/" + env.JOB_NAME.toString().split('/')[1] + '/'
     captain_json.("name") = env.JOB_NAME.toString()
     captain_json.("url") = pipeline_jobname
-    captain_json.("build") = [: ]
+    /*captain_json.("build") = [: ]
     captain_json.("build").("ful_url") = env.JOB_URL
     captain_json.("build").("number") = env.BUILD_ID
     captain_json.("build").("phase") = "STARTED"
@@ -17,6 +17,6 @@ def create_captain_call_file(){
     captain_json.("build").("parameters") = [: ]
     for (p in params){
         captain_json.("build").("parameters").(p.key.toString()) = (p.value.toString())
-    }
+    }*/
     writeJSON(file: captain_callback_file, json: captain_json, pretty: 4)
 }
