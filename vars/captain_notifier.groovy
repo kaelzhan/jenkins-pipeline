6c6204634f81e0a5d7110ad5820491e6aece589a
Kael Zhan <kzhan@kzhanmbp.local>
2018-12-12 00:45:44 +0800

    test

--- vars/captain_notifier.groovy
+++ vars/captain_notifier.groovy
@@ -3,7 +3,7 @@
 import env_config.Pipeline_Parameters
 import groovy.json.JsonOutput
 
-/*
+
 def create_captain_call_file(){
     def captain_callback_file = env.WORKSPACE + "/" + Pipeline_Parameters.captain_callback_file_name
     def captain_json = [: ]
@@ -18,21 +18,28 @@ def create_captain_call_file(){
     captain_json.("build").("status") = "SUCCESS"
     captain_json.("build").("parameters") = [: ]
     for (p in params){
-        captain_json.(p.key.toString()) = (p.value.toString())
+        captain_json.("build").("parameters").(p.key.toString()) = (p.value.toString())
     }
+    captain_json = readJSON text: groovy.json.JsonOutput.toJson(captain_json)
     writeJSON(file: captain_callback_file, json: captain_json, pretty: 4)
-}*/
+}
 
-// save all parameters from caption to local file and runtime_env
-def create_captain_call_file(){
+/*def create_captain_call_file(){
     def parameter_file = Pipeline_Parameters.captain_callback_file_name
     def parameter_file_local = env.WORKSPACE + "/" + parameter_file
-    def parameter_json = [: ]
+    def captain_json = [: ]
+
+    captain_json.("name") = env.JOB_NAME.toString()
+    captain_json.("build") = [: ]
+    captain_json.("build").("ful_url") = env.JOB_URL.toString()
+    captain_json.("build").("number") = env.BUILD_ID.toString()
+    captain_json.("build").("phase") = "STARTED"
+    captain_json.("build").("status") = "SUCCESS"
+    captain_json.("build").("parameters") = [: ]
 
     for( p in params ) {
-        parameter_json.(p.key.toString()) = (p.value.toString())
-        env.(p.key.toString()) = (p.value.toString())
+        captain_json.("build").("parameters").(p.key.toString()) = (p.value.toString())
     }
-    parameter_json = readJSON text: groovy.json.JsonOutput.toJson(parameter_json)
-    writeJSON(file: parameter_file_local, json: parameter_json, pretty: 4)
-}
+    captain_json = readJSON text: groovy.json.JsonOutput.toJson(captain_json)
+    writeJSON(file: parameter_file_local, json: captain_json, pretty: 4)
+}*/
