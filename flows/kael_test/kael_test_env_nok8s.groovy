1ea214cd4a1ffca882aa02413f28991b6906799f
Kael Zhan <kzhan@kzhanmbp.local>
2018-12-11 15:13:15 +0800

    test

--- flows/kael_test/kael_test_env_nok8s.groovy
+++ flows/kael_test/kael_test_env_nok8s.groovy
@@ -15,9 +15,9 @@ pipeline {
                 /*script{
                     captain_notifier.captain_call()
                 }*/
-                
+
                     jobDsl scriptText: '''
-                    pipelineJob('kael_test_env_nok8s/job/master') {
+                    pipelineJob('kael_test_env_nok8s') {
                         notifications {
                             endpoint(\'http://jenkins:lko34kd9fd2@captain.bbpd.io/api/jenkins/callback\') {
                                 event(\'all\')
