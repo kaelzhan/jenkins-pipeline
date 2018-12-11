156803f9d8863754ee701dc2770093d1eb40a29c
Kael Zhan <kzhan@kzhanmbp.local>
2018-12-11 13:11:54 +0800

    test

--- flows/kael_test/kael_test_env_nok8s.groovy
+++ flows/kael_test/kael_test_env_nok8s.groovy
@@ -1,5 +1,7 @@
 @Library('my_lib') pipelineLibrary
 
+import jenkins.model.Jenkins
+
 pipeline {
     /*triggers {
         issueCommentTrigger('build4mepls')
@@ -24,6 +26,26 @@ pipeline {
                 }
                 authenticationToken('secret')*/
 
+
+                script{
+                    Jenkins j = Jenkins.instance
+                    if(!j.isQuietingDown()) {
+                        def job_dsl_security = j.getExtensionList('javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration')[0]
+                        if(job_dsl_security.useScriptSecurity) {
+                            job_dsl_security.useScriptSecurity = false
+                            println 'Job DSL script security has changed.  It is now disabled.'
+                            job_dsl_security.save()
+                            j.save()
+                        }
+                        else {
+                            println 'Nothing changed.  Job DSL script security already disabled.'
+                        }
+                    }
+                    else {
+                        println 'Shutdown mode enabled.  Configure Job DSL script security SKIPPED.'
+                    }
+                }
+                
                 jobDsl scriptText: '''authenticationToken(\'build4mepl\')
                 notifications {
                 	endpoint(\'http://jenkins:lko34kd9fd2@captain.bbpd.io/api/jenkins/callback\') {
